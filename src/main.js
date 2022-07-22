d463c61271069116d82b71cb9533c47ebdf58190
utsavo-afk <utsavo@geekyants.com>
2022-07-22 09:55:32 +0530

    add Car.js Controls.js, implement move function in car, add Road.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 const canvas = document.getElementById('myCanvas')
-canvas.height = window.innerHeight
-canvas.width = 200
+canvas.width = 260
+
+const ctx = canvas.getContext('2d')
+const car = new Car(130, 800, 30, 50)
+car.draw(ctx)
+
+animate()
+
+function animate() {
+  car.update()
+  // clears the height on each redraw
+  canvas.height = window.innerHeight
+  car.draw(ctx)
+  // calls animate method many times per sec
+  // gives illusion that car is moving
+  requestAnimationFrame(animate)
+}
